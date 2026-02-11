12431460c2e9050f72ef22523830d1bfffee0d78
Anthony Rompre <anthonyrompre@gmail.com>
2026-02-10 19:16:21 -0500

    removed redundant lobbyid from lobbyclosed event

--- src/matchmaking/domain/lobby/events/LobbyClosed.ts
+++ src/matchmaking/domain/lobby/events/LobbyClosed.ts
@@ -2,10 +2,7 @@ import { DomainEvent } from '#common/domain/events/DomainEvent';
 import { LobbyId } from '../lobbyId/LobbyId';
 
 export class LobbyClosed extends DomainEvent {
-    public readonly lobbyId: LobbyId;
-
     constructor(lobbyId: LobbyId, schemaVersion = 1, occurredAt?: Date) {
         super(lobbyId.value, schemaVersion, occurredAt);
-        this.lobbyId = lobbyId;
     }
 }
