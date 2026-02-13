aed64f77b65351de265eddc09d1cd162c5a7c0a3
Michaël Panadis-Boisclair <46973666+panadis@users.noreply.github.com>
2026-02-13 01:13:37 -0500

    Début interface http nest et wiring

---
+++ src/matchmaking/interface/http/lobby/lobby.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { CreateLobbyRequest } from './request/CreateLobbyRequest';
+import { CreateLobbyUseCase } from '#matchmaking/usecase/CreateLobbyUseCase';
+import { CreateLobbyDto } from '#matchmaking/usecase/dto/CreateLobbyDto';
+import { LobbyId } from '#matchmaking/domain/lobby/lobbyId/LobbyId';
+import { GetLobbyUseCase } from '#matchmaking/usecase/GetLobbyUseCase';
+
+@Controller('lobby')
+export class LobbyController {
+    constructor(
+        private readonly createLobby: CreateLobbyUseCase,
+        private readonly getLobby: GetLobbyUseCase
+    ) {}
+
+    @Post()
+    create(@Body() createLobbyRequest: CreateLobbyRequest) {
+        const test: CreateLobbyDto = {
+            hostName: createLobbyRequest.hostName,
+            gameMode: createLobbyRequest.gameMode,
+        };
+
+        return this.createLobby.execute(test).createdLobby.id;
+    }
+
+    @Get(':id')
+    getLobbyById(@Param('id') id: string) {
+        return this.getLobby.execute({ lobbyId: new LobbyId(id) }).allPlayers;
+    }
+}
